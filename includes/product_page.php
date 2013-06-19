0f15a060c2b818338a3100b8792a9fc6fd99028d
unknown <nancy.chen@44397-501-WIN.auckland.natcoll.ac.nz>
2013-06-19 15:28:10 +1200

    View Cart link added to public index

--- includes/product_page.php
+++ includes/product_page.php
@@ -1,6 +1,7 @@
 <?php   
 
 	echo '<div class="main">';
+	echo '<a href="view_cart.php" class="viewCart">(0) View Cart</a>'; 
 	echo '<h2>'.$page->title.'</h2>'; 
 
 	echo '<p class="intro">'.$page->content.'';
